eb32b6578b663a56749772091995507e0ea4bc00
anurag-bhujbal <anuragbhujbal13@gmail.com>
2024-03-26 23:37:55 +0530

    Added Middleware and function to handle errors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -13,10 +14,22 @@ mongoose
   });
 
 
-const app = express();
+  const app = express();
+  app.use(express.json());
 
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!');
   });
   
+  app.use('/api/auth', authRoutes);
+
+  app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
